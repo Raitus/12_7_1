41f4bcca9e2c97aab1a2c98a2ed7293d8ca6a56e
Raitus <rd.raitus@gmail.com>
2021-07-02 08:41:51 +0300

    Added new checks to prevent wrong input.

--- main.cpp
+++ main.cpp
@@ -10,22 +10,30 @@ int main() {
 
 	startHours = 10*(startTime[0] - '0') + (startTime[1] - '0');
 	startMinutes = 10*(startTime[3] - '0') + (startTime[4] - '0');
-	if (startHours > 24 || startHours < 0 || startMinutes > 59 || startMinutes < 0 || startTime[2]!=':') {
+	if (startTime.length() > 5 || startTime[0] < '0' || startTime[0] > '9' || startTime[1] < '0' || startTime[1] > '9'
+		|| startTime[2]!=':' || startTime[3] < '0' || startTime[3] > '9' || startTime[4] < '0' || startTime[4] > '9'
+		|| startHours > 24 || startHours < 0 || startMinutes > 59 || startMinutes < 0) {
 	  std::cout << "Error! Please, input correct time at the time format HH:MM.\n";
 	}
-  } while (startHours > 24 || startHours < 0 || startMinutes > 59 || startMinutes < 0 || startTime[2]!=':');
+  } while (startTime.length() > 5 || startTime[0] < '0' || startTime[0] > '9' || startTime[1] < '0'
+	  || startTime[1] > '9' || startTime[2]!=':' || startTime[3] < '0' || startTime[3] > '9' || startTime[4] < '0'
+	  || startTime[4] > '9' || startHours > 24 || startHours < 0 || startMinutes > 59 || startMinutes < 0);
 
   do {
 	std::cout << "Input arrival time: ";
 	std::cin >> arrivingTime;
 	arrivingHours = 10*(arrivingTime[0] - '0') + (arrivingTime[1] - '0');
 	arrivingMinutes = 10*(arrivingTime[3] - '0') + (arrivingTime[4] - '0');
-	if (arrivingHours > 24 || arrivingHours < 0 || arrivingMinutes > 59 || arrivingMinutes < 0
-		|| arrivingTime[2]!=':') {
+	if (arrivingTime.length() > 5 || arrivingTime[0] < '0' || arrivingTime[0] > '9' || arrivingTime[1] < '0'
+		|| arrivingTime[1] > '9' || arrivingTime[2]!=':' || arrivingTime[3] < '0' || arrivingTime[3] > '9'
+		|| arrivingTime[4] < '0' || arrivingTime[4] > '9' || arrivingHours > 24 || arrivingHours < 0
+		|| arrivingMinutes > 59 || arrivingMinutes < 0) {
 	  std::cout << "Error! Please, input correct time at the time format HH:MM.\n";
 	}
-  } while (arrivingHours > 24 || arrivingHours < 0 || arrivingMinutes > 59 || arrivingMinutes < 0
-	  || arrivingTime[2]!=':');
+  } while (arrivingTime.length() > 5 || arrivingTime[0] < '0' || arrivingTime[0] > '9' || arrivingTime[1] < '0'
+	  || arrivingTime[1] > '9' || arrivingTime[2]!=':' || arrivingTime[3] < '0' || arrivingTime[3] > '9'
+	  || arrivingTime[4] < '0' || arrivingTime[4] > '9' || arrivingHours > 24 || arrivingHours < 0
+	  || arrivingMinutes > 59 || arrivingMinutes < 0);
 
   std::cout << "You will be in the way ";
   int initialMinutes = startHours*60 + startMinutes;
