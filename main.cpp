8c5b0ab7693f406ce6e90ac763f66936efe8e647
Raitus <rd.raitus@gmail.com>
2021-07-04 10:13:30 +0300

    corrected checks sequence

--- main.cpp
+++ main.cpp
@@ -4,36 +4,45 @@ int main() {
   std::cout << "Input the time in HH:MM format." << std::endl;
   std::string startTime, arrivingTime;
   int startHours, startMinutes, arrivingHours, arrivingMinutes;
-  do {
-	std::cout << "Input start time: ";
-	std::cin >> startTime;
+  {
+	bool check = false;
+	do {
+	  std::cout << "Input start time: ";
+	  std::cin >> startTime;
 
-	startHours = 10*(startTime[0] - '0') + (startTime[1] - '0');
-	startMinutes = 10*(startTime[3] - '0') + (startTime[4] - '0');
-	if (startTime.length() > 5 || startTime[0] < '0' || startTime[0] > '9' || startTime[1] < '0' || startTime[1] > '9'
-		|| startTime[2]!=':' || startTime[3] < '0' || startTime[3] > '9' || startTime[4] < '0' || startTime[4] > '9'
-		|| startHours > 24 || startHours < 0 || startMinutes > 59 || startMinutes < 0) {
-	  std::cout << "Error! Please, input correct time at the time format HH:MM.\n";
-	}
-  } while (startTime.length() > 5 || startTime[0] < '0' || startTime[0] > '9' || startTime[1] < '0'
-	  || startTime[1] > '9' || startTime[2]!=':' || startTime[3] < '0' || startTime[3] > '9' || startTime[4] < '0'
-	  || startTime[4] > '9' || startHours > 24 || startHours < 0 || startMinutes > 59 || startMinutes < 0);
-
-  do {
-	std::cout << "Input arrival time: ";
-	std::cin >> arrivingTime;
-	arrivingHours = 10*(arrivingTime[0] - '0') + (arrivingTime[1] - '0');
-	arrivingMinutes = 10*(arrivingTime[3] - '0') + (arrivingTime[4] - '0');
-	if (arrivingTime.length() > 5 || arrivingTime[0] < '0' || arrivingTime[0] > '9' || arrivingTime[1] < '0'
-		|| arrivingTime[1] > '9' || arrivingTime[2]!=':' || arrivingTime[3] < '0' || arrivingTime[3] > '9'
-		|| arrivingTime[4] < '0' || arrivingTime[4] > '9' || arrivingHours > 24 || arrivingHours < 0
-		|| arrivingMinutes > 59 || arrivingMinutes < 0) {
-	  std::cout << "Error! Please, input correct time at the time format HH:MM.\n";
-	}
-  } while (arrivingTime.length() > 5 || arrivingTime[0] < '0' || arrivingTime[0] > '9' || arrivingTime[1] < '0'
-	  || arrivingTime[1] > '9' || arrivingTime[2]!=':' || arrivingTime[3] < '0' || arrivingTime[3] > '9'
-	  || arrivingTime[4] < '0' || arrivingTime[4] > '9' || arrivingHours > 24 || arrivingHours < 0
-	  || arrivingMinutes > 59 || arrivingMinutes < 0);
+	  if (startTime.length()!=5 || startTime[0] < '0' || startTime[0] > '9' || startTime[1] < '0' || startTime[1] > '9'
+		  || startTime[2]!=':' || startTime[3] < '0' || startTime[3] > '9' || startTime[4] < '0'
+		  || startTime[4] > '9') {
+		std::cout << "Error! Please, input correct time at the time format HH:MM.\n";
+	  } else {
+		startHours = 10*(startTime[0] - '0') + (startTime[1] - '0');
+		startMinutes = 10*(startTime[3] - '0') + (startTime[4] - '0');
+		if (startHours > 24 || startHours < 0 || startMinutes > 59 || startMinutes < 0) {
+		  std::cout << "Error! Please, input correct time at the time format HH:MM.\n";
+		} else {
+		  check = true;
+		}
+	  }
+	} while (check==false);
+	check = false;
+	do {
+	  std::cout << "Input arrival time: ";
+	  std::cin >> arrivingTime;
+	  if (arrivingTime.length()!=5 || arrivingTime[0] < '0' || arrivingTime[0] > '9' || arrivingTime[1] < '0'
+		  || arrivingTime[1] > '9' || arrivingTime[2]!=':' || arrivingTime[3] < '0' || arrivingTime[3] > '9'
+		  || arrivingTime[4] < '0' || arrivingTime[4] > '9') {
+		std::cout << "Error! Please, input correct time at the time format HH:MM.\n";
+	  } else {
+		arrivingHours = 10*(arrivingTime[0] - '0') + (arrivingTime[1] - '0');
+		arrivingMinutes = 10*(arrivingTime[3] - '0') + (arrivingTime[4] - '0');
+		if (arrivingHours > 24 || arrivingHours < 0 || arrivingMinutes > 59 || arrivingMinutes < 0) {
+		  std::cout << "Error! Please, input correct time at the time format HH:MM.\n";
+		} else {
+		  check = true;
+		}
+	  }
+	} while (check==false);
+  }
 
   std::cout << "You will be in the way ";
   int initialMinutes = startHours*60 + startMinutes;
@@ -47,6 +56,5 @@ int main() {
 	int minutes = (1440 - initialMinutes + eventualMinutes) - hours*60;
 	std::cout << hours << " hours and " << minutes << " minutes.";
   }
-
   return 0;
 }
